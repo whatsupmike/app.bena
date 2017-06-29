33676ba29f910d157f473cb4c01561ee5859d850
= <mdrzewiecki91@gmail.com>
2017-06-29 15:31:55 +0200

    refueling backend addded

--- app/Http/Controllers/FuelController.php
+++ app/Http/Controllers/FuelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Fuel;
 use App\Car;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FuelController extends Controller
 {
@@ -44,7 +45,37 @@ class FuelController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'car_id' => 'required',
+            'fuelQuantity' => 'required|numeric',
+            'fuelPrice' => 'required|numeric']);
+
+
+        if(!$request->exists('isFullFueling')){
+            $isFullFueling = 0;
+            $lastFullFueling = Fuel::lastFullFueling($request->car_id)->fuel_id;
+
+        }
+        else if ($request->isFullFueling == 1){
+            $isFullFueling = 1;
+            $lastFullFueling = null;
+        }
+
+        $fuel = new Fuel();
+
+        $fuel->car_id = $request->car_id;
+        $fuel->user_id = Auth::id();
+        $fuel->fuelQuantity = $request->fuelQuantity;
+        $fuel->fuelPrice = $request->fuelPrice;
+        $fuel->fuelValue = number_format(($request->fuelQuantity*$request->fuelPrice), 2, '.', '');
+
+        $fuel->isFullFueling = $isFullFueling;
+        $fuel->lastFullFueling = $lastFullFueling;
+
+        $fuel->save();
+
+        return redirect()->route('trip.create');
+
     }
 
     /**
