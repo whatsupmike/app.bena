51b25db90244272a1ffd905e017ffd5a202727cf
= <mdrzewiecki91@gmail.com>
2017-06-29 15:28:40 +0200

    global helper functions added (js)

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,3 +25,4 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/libs/messages.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css').version();
 
+mix.copy('resources/assets/js/helpers.js', 'public/js/helpers.js');
