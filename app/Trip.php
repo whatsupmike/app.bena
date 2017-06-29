1d709c365f7355a84ad5f60aeed6e033c80496a6
= <mdrzewiecki91@gmail.com>
2017-06-29 09:56:15 +0200

    Fuel added to trip table (model+ migration) + code formatting

--- app/Trip.php
+++ app/Trip.php
@@ -1,49 +1,59 @@
 <?php
 
-namespace App;
+    namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Cviebrock\EloquentSluggable\Sluggable;
-use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
+    use Cviebrock\EloquentSluggable\Sluggable;
+    use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
-class Trip extends Model
-{
-    use SoftDeletes, Sluggable, SluggableScopeHelpers;
-    protected $primaryKey = 'trip_id';
-
-
-
-    /**
-     * Get car assigned to trip
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function car(){
-        return $this->belongsTo('App\Car', 'car_id', 'car_id');
-    }
-
-    /**
-     * Get user assigned to trip
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function user(){
-        return $this->belongsTo('App\User', 'user_id', 'user_id');
-    }
-
-    /**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable()
+    class Trip extends Model
     {
-        return [
-            'slug' => [
-                'source' => ['odometerBefore', 'car_id'],
-                'separator' => '-'
-            ]
-        ];
+        use SoftDeletes, Sluggable, SluggableScopeHelpers;
+        protected $primaryKey = 'trip_id';
+
+        /**
+         * Get fueling assigned to trip
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+         */
+        public function fueling()
+        {
+            return $this->belongsTo('App\Fuel', 'fuel_id', 'fuel_id');
+        }
+
+        /**
+         * Get car assigned to trip
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+         */
+        public function car()
+        {
+            return $this->belongsTo('App\Car', 'car_id', 'car_id');
+        }
+
+        /**
+         * Get user assigned to trip
+         *
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+         */
+        public function user()
+        {
+            return $this->belongsTo('App\User', 'user_id', 'user_id');
+        }
+
+        /**
+         * Return the sluggable configuration array for this model.
+         *
+         * @return array
+         */
+        public function sluggable()
+        {
+            return [
+                'slug' => [
+                    'source' => ['odometerBefore', 'car_id'],
+                    'separator' => '-'
+                ]
+            ];
+        }
     }
-}
