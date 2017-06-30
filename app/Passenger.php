0fab4c6dcee9b8b3cd0bdd2ea3fe431c8a3eb7c7
= <mdrzewiecki91@gmail.com>
2017-06-30 16:49:56 +0200

    Passengers model + migration

---
+++ app/Passenger.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+
+class Passenger extends Model
+{
+    use SoftDeletes, Sluggable, SluggableScopeHelpers;
+    protected $primaryKey = 'passenger_id';
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' =>  'name'
+            ]
+        ];
+    }
+
+}
